377903def647b279a72c9a4a9c988089c4ad0494
Robert Ramirez <robertpramirez44@gmail.com>
2024-04-28 21:48:53 -0600

    created xcode project
    Initial project creation

---
+++ Step Tracker/Step_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Step_TrackerApp.swift
+//  Step Tracker
+//
+//  Created by Robert Ramirez on 4/28/24.
+//
+
+import SwiftUI
+
+@main
+struct Step_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
