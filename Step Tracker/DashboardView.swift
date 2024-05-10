7f180c296b58a3b932f9efd8f36cf8707112ae6a
Robert Ramirez <robertpramirez44@gmail.com>
2024-05-09 21:39:15 -0600

    step bar chart styling

--- Step Tracker/DashboardView.swift
+++ Step Tracker/DashboardView.swift
@@ -28,7 +28,13 @@ struct DashboardView: View {
   @State private var isShowingPermissionPrimingSheet = false
   @State private var selectedStat: HealthMetricContext = .steps
   var isSteps: Bool { selectedStat == .steps}
-  
+
+  var avgStepCount: Double {
+    guard !hkManager.stepData.isEmpty else { return 0 }
+    let totalSteps = hkManager.stepData.reduce(0) { $0 + $1.value }
+    return totalSteps/Double(hkManager.stepData.count)
+  }
+
   var body: some View {
     NavigationStack {
       ScrollView {
@@ -49,7 +55,7 @@ struct DashboardView: View {
                     .font(.title.bold())
                     .foregroundStyle(.pink)
                   
-                  Text("Avg: 10k Steps")
+                  Text("Avg: \(Int(avgStepCount)) Steps")
                     .font(.caption)
                 }
                 
@@ -62,14 +68,34 @@ struct DashboardView: View {
             .padding(.bottom, 12)
             
             Chart {
+              RuleMark(y: .value("Average", avgStepCount))
+                .foregroundStyle(Color.secondary)
+                .lineStyle(.init(lineWidth: 1, dash: [5]))
+
               ForEach(hkManager.stepData) { steps in
                 BarMark(
                   x: .value("Date", steps.date, unit: .day),
                   y: .value("Steps", steps.value)
                 )
+                .foregroundStyle(Color.pink.gradient)
               }
             }
             .frame(height: 150)
+            .chartXAxis {
+              AxisMarks {
+                AxisValueLabel(format: .dateTime.month(.defaultDigits).day())
+              }
+            }
+            .chartYAxis {
+              AxisMarks { value in
+                AxisGridLine()
+                  .foregroundStyle(Color.secondary.opacity(0.3))
+
+                AxisValueLabel((value.as(Double.self) ?? 0
+                  ).formatted(.number.notation(.compactName))
+                )
+              }
+            }
           }
           .padding()
           .background(RoundedRectangle(cornerRadius: 12).fill(Color(.secondarySystemBackground)))
